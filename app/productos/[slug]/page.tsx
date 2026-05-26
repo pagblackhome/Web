cd183d055225b97e48b025b302f0050854acef1b
pagblackhome <pag.blackhome@gmail.com>
2026-05-26 00:45:06 -0400

    Add files via upload

--- app/productos/[slug]/page.tsx
+++ app/productos/[slug]/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-//import CurtainSimulator from "components/CurtainSimulator";
+import CurtainSimulator from "@/components/CurtainSimulator";
 
 
 

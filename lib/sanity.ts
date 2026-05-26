ffd60e2a40bdcf2249d66d581742699b6126c3cb
pagblackhome <pag.blackhome@gmail.com>
2026-05-25 23:20:02 -0400

    Delete lib directory

--- lib/sanity.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const client = createClient({
-  projectId: "6vsy5lg8",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: true,
-});

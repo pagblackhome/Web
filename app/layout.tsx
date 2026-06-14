f58f17274f6ac5350bb8816f65fe0a01fb6947f9
pagblackhome <pag.blackhome@gmail.com>
2026-06-14 13:32:05 -0400

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,22 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Black Home | Cortinas Modernas y Decoración en Chile",
+
   description:
     "Cortinas roller, blackout, duo y motorizadas en Chile. Diseño moderno y elegante.",
+
+  icons: {
+    icon: "/BH.png",
+    shortcut: "/BH.png",
+    apple: "/BH.png",
+  },
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="es">
       <body className="bg-white text-black">
